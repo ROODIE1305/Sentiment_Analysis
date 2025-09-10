3b539a94da7f5f9d33dde132f5dc9a0871a998c8
Rudra <khandelwalrudra1305@gmail.com>
2025-09-10 15:33:48 +0530

    messasge to live backend service

--- backend/routes.py
+++ backend/routes.py
@@ -4,6 +4,17 @@ from backend.sentiments import check_sentiment
 
 router = APIRouter()
 
+@router.get("/")
+def read_root():
+    return {
+        "message": "Welcome to My Sentiment Analysis API 🎉",
+        "About": "Live backend service for my Sentiment_Analysis Project backed by Render ",
+        "endpoints": {
+            "Analyze Sentiment": "/analyze",
+            "API Docs": "/docs"
+        }
+    }
+    
 @router.post("/analyze")
 def analyze_feedback(feedback: Feedback):
     sentiment = check_sentiment(feedback.text)
